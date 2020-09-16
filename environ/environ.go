2f93781d72761125c7eb3a2176413f188c01fbe2
licong <licong@qianxin.com>
2020-09-16 16:52:45 +0800

    init

--- environ/environ.go
+++ environ/environ.go
@@ -1,6 +1,34 @@
 package environ
 
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
 // GetEnv Get process enviroment list
 func GetEnv(pid int) (env map[string]string, err error) {
-	return nil, nil
+	file := fmt.Sprintf("/proc/%d/environ", pid)
+	fh, err := os.Open(file)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("can not open file %s:%s", file, err))
+	}
+	env = make(map[string]string)
+	var reader = bufio.NewReader(fh)
+	for {
+		line, err := reader.ReadSlice(byte(0))
+		if err != nil {
+			break
+		}
+
+		keyValue := strings.Split(string(line), "=")
+		if len(keyValue) < 2 {
+			env[keyValue[0]] = ""
+		} else {
+			env[keyValue[0]] = keyValue[1]
+		}
+	}
+	return env, nil
 }
--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+
 	"github.com/hellojukay/proc/environ"
 )
 
+var enableEnv = false
+var enableTcp = false
+var pid = 0
+
+func init() {
+	flag.BoolVar(&enableEnv, "e", false, "show process environment list")
+	flag.BoolVar(&enableTcp, "t", false, "show process tcp infomation")
+	flag.IntVar(&pid, "p", 1, "process pid")
+	flag.Parse()
+}
 func main() {
-	m, err := environ.GetEnv(1)
+	m, err := environ.GetEnv(pid)
 	if err != nil {
 		panic(err)
 	}
 	for k, v := range m {
-		fmt.Printf("%10s%30s\n", k, v)
+		fmt.Printf("%-20s%-30s\n", k, v)
 	}
 }
